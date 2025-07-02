876c0406841258893e71bb198d6c63875c67b0f9
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-07-02 22:04:03 +0200

    fix backup script to use absolute paths

--- backup.sh
+++ backup.sh
@@ -34,14 +34,17 @@ if [ -n "${BACKUP_MAX_BEFORE_DELETE}" ]; then
 fi
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+ABSOLUTE_BACKUP_DIR="/home/backupuser/${BACKUP_DIR}"
 BACKUP_FILE="${BACKUP_DIR}/${TIMESTAMP}.sql"
+ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_DIR}/${TIMESTAMP}.sql"
 
 # Create the backup directory if it doesn't exist
-mkdir -p "${BACKUP_DIR}"
+mkdir -p "${ABSOLUTE_BACKUP_DIR}"
+echo "Folder '${ABSOLUTE_BACKUP_FILE}' created."
 
 # Save the database
 echo "Saving database ${DB_NAME}..."
-pg_dump_output=$(PGPASSWORD=${DB_PASSWORD} pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${DB_NAME} -F p -c -v -f ${BACKUP_FILE} 2>&1)
+pg_dump_output=$(PGPASSWORD=${DB_PASSWORD} pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${DB_NAME} -F p -c -v -f ${ABSOLUTE_BACKUP_FILE} 2>&1)
 
 if [ $? -ne 0 ]; then
   echo "Error: Failed to save the database. Details:"
@@ -55,21 +58,23 @@ if [ -n "${BACKUP_COMPRESSION}" ]; then
   case "${BACKUP_COMPRESSION}" in
     gzip)
       echo "Compressing backup with gzip..."
-      gzip -f ${BACKUP_FILE}
+      gzip -f ${ABSOLUTE_BACKUP_FILE}
+      ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_FILE}.gz"
       BACKUP_FILE="${BACKUP_FILE}.gz"
-      ;;
-    bzip2)
-      echo "Compressing backup with bzip2..."
-      bzip2 -f ${BACKUP_FILE}
-      BACKUP_FILE="${BACKUP_FILE}.bz2"
+      echo "Backup compressed with gzip."
       ;;
     xz)
       echo "Compressing backup with xz..."
-      xz -f ${BACKUP_FILE}
+      xz -f ${ABSOLUTE_BACKUP_FILE}
+      ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_FILE}.xz"
       BACKUP_FILE="${BACKUP_FILE}.xz"
+      echo "Backup compressed with xz."
       ;;
     *)
       echo "Warning: Unsupported compression method '${BACKUP_COMPRESSION}'. No compression applied."
+      echo "Deleting local backup file ${BACKUP_FILE}..."
+      rm -f "${ABSOLUTE_BACKUP_FILE}"
+      exit 1
       ;;
   esac
 else
@@ -78,7 +83,7 @@ fi
 
 # Upload to S3
 echo "Uploading to S3..."
-s3cmd_put_output=$(s3cmd put ${BACKUP_FILE} s3://${S3_BUCKET}/${BACKUP_FILE} \
+s3cmd_put_output=$(s3cmd put ${ABSOLUTE_BACKUP_FILE} s3://${S3_BUCKET}/${BACKUP_FILE} \
   --access_key=${S3_ACCESS_TOKEN} \
   --secret_key=${S3_SECRET_ACCESS_TOKEN} \
   --host=${S3_ENDPOINT} \
@@ -88,7 +93,7 @@ if [ $? -ne 0 ]; then
   echo "Error: Failed to upload to S3. Details:"
   echo "${s3cmd_put_output}"
   echo "Deleting local backup file ${BACKUP_FILE}..."
-  rm -f "${BACKUP_FILE}"
+  rm -f "${ABSOLUTE_BACKUP_FILE}"
   exit 1
 fi
 
@@ -124,4 +129,4 @@ else
 fi
 
 echo "Deleting local backup file ${BACKUP_FILE}..."
-rm -f "${BACKUP_FILE}"
+rm -f "${ABSOLUTE_BACKUP_FILE}"

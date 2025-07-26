c6eadd2d83c1e23f2702f1ffbfe80071bc422977
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-07-26 13:53:39 +0200

    handle compression type

--- backup.sh
+++ backup.sh
@@ -18,8 +18,28 @@ S3_BUCKET=${S3_BUCKET}
 
 # Validate environment variables
 if [ -z "${DB_HOST}" ] || [ -z "${DB_USER}" ] || [ -z "${DB_PASSWORD}" ] || [ -z "${DB_NAME}" ] || [ -z "${S3_ENDPOINT}" ] || [ -z "${S3_ACCESS_TOKEN}" ] || [ -z "${S3_SECRET_ACCESS_TOKEN}" ] || [ -z "${S3_BUCKET}" ]; then
-  echo "Error: Missing environment variables"
+  echo "Error: Missing environment variables."
   exit 1
+fi
+
+# Validate BACKUP_DIR
+if [ -n "${BACKUP_DIR}" ]; then
+    echo "Validating BACKUP_DIR environment variable..."
+    
+    if [[ ! "${BACKUP_DIR}" =~ ^[a-zA-Z0-9_/-]+$ ]]; then
+        echo "Error: BACKUP_DIR can only contain alphanumeric characters, underscores, slashes and hyphens."
+        exit 1
+    fi
+
+    if [[ "${BACKUP_DIR}" == /* ]]; then
+        echo "Warning: BACKUP_DIR must not start with a slash."
+        BACKUP_DIR=$(echo "${BACKUP_DIR}" | sed 's|^/||')
+    fi
+
+    if [[ "${BACKUP_DIR}" == */ ]]; then
+        echo "Warning: BACKUP_DIR must not end with a slash."
+        BACKUP_DIR=$(echo "${BACKUP_DIR}" | sed 's|/$||')
+    fi
 fi
 
 # Validate BACKUP_MAX_BEFORE_DELETE
@@ -33,10 +53,17 @@ if [ -n "${BACKUP_MAX_BEFORE_DELETE}" ]; then
   fi
 fi
 
+# Create the file name
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+# Create the container backup directory
 ABSOLUTE_BACKUP_DIR="/home/backupuser/${BACKUP_DIR}"
-BACKUP_FILE="${BACKUP_DIR}/${TIMESTAMP}.sql"
 ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_DIR}/${TIMESTAMP}.sql"
+
+if [ -z "${BACKUP_DIR}" ]; then
+    S3_BACKUP_FILE="${TIMESTAMP}.sql"
+else
+    S3_BACKUP_FILE="${BACKUP_DIR}/${TIMESTAMP}.sql"
+fi
 
 # Create the backup directory if it doesn't exist
 mkdir -p "${ABSOLUTE_BACKUP_DIR}"
@@ -52,27 +79,55 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "Successful backup: ${BACKUP_FILE}"
+echo "Successful backup in '${S3_BACKUP_FILE}'."
 
 if [ -n "${BACKUP_COMPRESSION}" ]; then
   case "${BACKUP_COMPRESSION}" in
     gzip)
       echo "Compressing backup with gzip..."
-      gzip -f ${ABSOLUTE_BACKUP_FILE}
+      compress_output=$(gzip -f ${ABSOLUTE_BACKUP_FILE} 2>&1)
+
+      if [ $? -ne 0 ]; then
+        echo "Error: Failed to compress the backup with gzip. Details:"
+        echo "${compress_output}"
+        exit 1
+      fi
+
       ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_FILE}.gz"
-      BACKUP_FILE="${BACKUP_FILE}.gz"
+      S3_BACKUP_FILE="${S3_BACKUP_FILE}.gz"
       echo "Backup compressed with gzip."
       ;;
     xz)
       echo "Compressing backup with xz..."
-      xz -f ${ABSOLUTE_BACKUP_FILE}
+      compress_output=$(xz -f ${ABSOLUTE_BACKUP_FILE} 2>&1)
+
+      if [ $? -ne 0 ]; then
+        echo "Error: Failed to compress the backup with xz. Details:"
+        echo "${compress_output}"
+        exit 1
+      fi
+
       ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_FILE}.xz"
-      BACKUP_FILE="${BACKUP_FILE}.xz"
+      S3_BACKUP_FILE="${S3_BACKUP_FILE}.xz"
       echo "Backup compressed with xz."
+      ;;
+    zip)
+      echo "Compressing backup with zip..."
+      compress_output=$(zip -r ${ABSOLUTE_BACKUP_FILE}.zip ${ABSOLUTE_BACKUP_FILE} 2>&1)
+
+      if [ $? -ne 0 ]; then
+        echo "Error: Failed to compress the backup with zip. Details:"
+        echo "${compress_output}"
+        exit 1
+      fi
+
+      ABSOLUTE_BACKUP_FILE="${ABSOLUTE_BACKUP_FILE}.zip"
+      S3_BACKUP_FILE="${S3_BACKUP_FILE}.zip"
+      echo "Backup compressed with zip."
       ;;
     *)
       echo "Warning: Unsupported compression method '${BACKUP_COMPRESSION}'. No compression applied."
-      echo "Deleting local backup file ${BACKUP_FILE}..."
+      echo "Deleting local backup file ${S3_BACKUP_FILE}..."
       rm -f "${ABSOLUTE_BACKUP_FILE}"
       exit 1
       ;;
@@ -83,7 +138,7 @@ fi
 
 # Upload to S3
 echo "Uploading to S3..."
-s3cmd_put_output=$(s3cmd put ${ABSOLUTE_BACKUP_FILE} s3://${S3_BUCKET}/${BACKUP_FILE} \
+s3cmd_put_output=$(s3cmd put ${ABSOLUTE_BACKUP_FILE} s3://${S3_BUCKET}/${S3_BACKUP_FILE} \
   --access_key=${S3_ACCESS_TOKEN} \
   --secret_key=${S3_SECRET_ACCESS_TOKEN} \
   --host=${S3_ENDPOINT} \
@@ -92,7 +147,7 @@ s3cmd_put_output=$(s3cmd put ${ABSOLUTE_BACKUP_FILE} s3://${S3_BUCKET}/${BACKUP_
 if [ $? -ne 0 ]; then
   echo "Error: Failed to upload to S3. Details:"
   echo "${s3cmd_put_output}"
-  echo "Deleting local backup file ${BACKUP_FILE}..."
+  echo "Deleting local backup file ${S3_BACKUP_FILE}..."
   rm -f "${ABSOLUTE_BACKUP_FILE}"
   exit 1
 fi
@@ -121,12 +176,12 @@ else
       echo "Error: Failed to delete ${FILE_PATH}. Details:"
       echo "${s3cmd_del_output}"
     else
-      echo "Successfully deleted ${FILE_PATH}"
+      echo "Successfully deleted '${FILE_PATH}'."
     fi
   done
 
-  echo "Old backups have been sorted"
+  echo "Old backups have been sorted."
 fi
 
-echo "Deleting local backup file ${BACKUP_FILE}..."
+echo "Deleting local backup file '${ABSOLUTE_BACKUP_FILE}'..."
 rm -f "${ABSOLUTE_BACKUP_FILE}"
